9fd9a1e9ab0354f3d4bf34b1139c9ec1dc028b5a
rohan-develop <rohan19.ry@gmail.com>
2020-04-01 23:00:11 +0530

    first commit

--- search_string_from_console.js
+++ search_string_from_console.js
@@ -1,1 +1,16 @@
-e
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const arr = ['rohan','neha','sibbu'];
+rl.question('Enter a name ', (answer) => {
+ if(arr.includes(answer)){
+     console.log('exists');
+ }
+ else {console.log("not found");}
+
+ 
+  rl.close();
+});
